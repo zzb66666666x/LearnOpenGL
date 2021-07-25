a8c3a34eb931c06260e3386465a893774a44cfca
zzb66666666x <18767066589@163.com>
2021-07-25 18:40:44 +0800

    updated

---
+++ nanosuit/build/CMakeFiles/nanosuit.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nanosuit.dir/glad.c.obj"
+  "CMakeFiles/nanosuit.dir/main.cpp.obj"
+  "libnanosuit.dll.a"
+  "nanosuit.exe"
+  "nanosuit.exe.manifest"
+  "nanosuit.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/nanosuit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
