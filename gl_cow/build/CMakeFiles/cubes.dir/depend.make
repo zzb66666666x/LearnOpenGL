6e6bb0b2c1fc7dc5f7a005ac982d179670bfdf37
Zhongbo Zhu <18767066589@163.com>
2024-04-01 22:13:50 -0500

    rm build

--- gl_cow/build/CMakeFiles/cubes.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for cubes.
-# This may be replaced when dependencies are built.
--- sponza/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/MyCode/Cpp/learnGL/sponza
-# Build directory: D:/MyCode/Cpp/learnGL/sponza/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
